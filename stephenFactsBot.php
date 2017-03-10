d0a94513e6a8495dd7f8a5cd01f77eb39f14cbc1
Corey Kress <coreykre@buffalo.edu>
2017-03-10 16:56:24 -0500

    added composer and chucknorris fact guzzler

--- stephenFactsBot.php
+++ stephenFactsBot.php
@@ -1,6 +1,7 @@
 <?php
-// Prevent PHP from stopping the script after 30 sec
 set_time_limit(0);
+
+use Bot\ChuckNorrisFactGuzzler;
 
 // Edit these settings
 $chan = "";
@@ -8,11 +9,14 @@ $server = "127.0.0.1";
 $port = 6667;
 $nick = "Stephen_Facts";
 
-// STOP EDITTING NOW.
 $socket = fsockopen("$server", $port);
 fputs($socket,"USER $nick $nick $nick $nick :$nick\n");
 fputs($socket,"NICK $nick\n");
 fputs($socket,"JOIN ".$chan."\n");
+
+$baseURI = 'https://api.chucknorris.io/jokes';
+$name = 'Stephen';
+$factGuzzler = new ChuckNorrisFactGuzzler($baseURI, $name);
 
 while(1) {
     while($data = fgets($socket)) {
@@ -35,8 +39,11 @@ while(1) {
 
         $args = NULL; for ($i = 4; $i < count($ex); $i++) { $args .= $ex[$i] . ' '; }
 
-            if ($rawcmd[1] == "!fact") {
-                fputs($socket, "PRIVMSG ".$channel." :".$args." \n");
+            if ($rawcmd[1] == "!random") {
+                /** @var string $fact */
+                $fact = $factGuzzler->getRandomFact();
+
+                fputs($socket, "PRIVMSG ".$channel." :".$fact." \n");
             }
         elseif ($rawcmd[1] == "!md5") {
             fputs($socket, "PRIVMSG ".$channel." :MD5 ".md5($args)."\n");
